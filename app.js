df465fc90d2d8394273b8e90c9b92fffb77d7e84
William Wang <williamblue543@gmail.com>
2024-02-06 11:50:37 -0500

    Mouse Animation

--- app.js
+++ app.js
@@ -3,3 +3,25 @@ screen.addEventListener("orientationchange", () => {
   });
 
   screen.orientation.lock("portrait");
+
+  const trailer = document.getElementById("trailer");
+
+const animateTrailer = (e, interacting) => {
+    const x = e.clientX,
+    y = e.clientY ;
+
+    const keyframes = {
+        transform: `translate(${x}px, ${y}px) scale(${interacting ? 6 : 1})`
+    }
+    trailer.animate(keyframes, {
+        duration: 800,
+        fill: "forwards"
+    });
+}
+
+window.onmousemove = e => {
+    const interactable = e.target.closest("#animate-text"),
+    interacting = interactable !== null; 
+    animateTrailer(e, interacting);
+    trailer.dataset.type = interacting ? interactable.dataset.type : "";
+}
